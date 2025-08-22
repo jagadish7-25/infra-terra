ce16d8f6b705f79abb68e987f16b9766c29ec493
jagadish7-25 <kadapajag9@gmail.com>
2025-08-22 15:43:56 +0530

    ec2 resource created

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,3 +22,12 @@ egress {
 }
 
 }
+resource "aws_instance" "terraform" {
+
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all_traffic.id]
+    tags = {
+        Name = "terraform"
+    }
+}
