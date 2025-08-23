f7f57262e15c8980fb77f9929e8cdbbcc0aa14eb
jagadish7-25 <kadapajag9@gmail.com>
2025-08-23 18:32:15 +0530

    s3 table created

--- loop/provider.tf
+++ loop/provider.tf
@@ -5,9 +5,16 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "chocojohns-25"
+    key    = "chocojohns-lock1"
+    region = "us-east-1"
+    dynamodb_table = "chocojohns"
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
