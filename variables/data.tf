8c3f8f12da5b228ffe7e9f281ca4ace33e5323fe
jagadish7-25 <kadapajag9@gmail.com>
2025-08-23 16:30:04 +0530

    datasource added

--- variables/data.tf
+++ variables/data.tf
@@ -1,3 +1,14 @@
-data "aws_instance" "example" {
-  instance_id = "<your-instance-id>"
+data "aws_ami" "finding" {
+  most_recent = true
+
+  owners = ["973714476881"]
+  tags = {
+    Name   = "terraform"
+    instace_type = "t2.micro"
+  }
+}
+
+output "finding" {
+  value = data.aws_ami.finding.id
+  
 }
