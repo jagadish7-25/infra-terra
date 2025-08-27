b92a3d8917cf18adecfac96c3126d6fff01e0d31
jagadish7-25 <kadapajag9@gmail.com>
2025-08-27 15:50:02 +0530

    addded

---
+++ multi/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "chocojohns-25-dev"
+key    = "chocojohns-dev1"
+region = "us-east-1"
+dynamodb_table = "chocojohns-25-dev"
---
+++ multi/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "chocojohns-25-prod"
+key    = "chocojohns-prod1"
+region = "us-east-1"
+dynamodb_table = "chocojohns-25-prod"
